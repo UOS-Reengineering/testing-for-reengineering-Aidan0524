9bac9c42b51a631a23daf4e27de6f1d76cbbcb6c
Guancong He <ghe4@sheffield.ac.uk>
2023-03-27 19:53:53 +0100

    Fix testRun2()

--- src/test/java/example/project/TestSimulator.java
+++ src/test/java/example/project/TestSimulator.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import example.project.domain.Scenario;
+import example.project.domain.SimulationResult;
 import org.junit.jupiter.api.Test;
 
 public class TestSimulator {
@@ -17,7 +18,10 @@ public class TestSimulator {
 
     @Test
     public void testRun2() {
-        fail();
+        Simulator simulator = new Simulator("dummy");
+        Scenario scenario =  new Scenario();
+        ADS ads = new ADS("dummy");
+        assertEquals(null, simulator.run(scenario, ads));
         // remove the above line and
         // complete a test case that calls `simulator.run(scenario, ads)` and verify if the result is null.
     }
